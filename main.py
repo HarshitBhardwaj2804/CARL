ac3b6d4220dafd80c9e1e41328d513fce9fdf243
Harshit Bhardwaj <130764185+HarshitBhardwaj2804@users.noreply.github.com>
2025-09-16 14:55:07 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -215,8 +215,6 @@ def pdf_chatbot():
 @app.route("/settings")
 def settings():
     return render_template("settings.html")
-    
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render sets PORT automatically
-    app.run(host="0.0.0.0", port=port, debug=True)
 
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
