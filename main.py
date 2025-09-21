06a80c26d3fabe851471419cde5baf12b4f3be28
Harshit Bhardwaj <130764185+HarshitBhardwaj2804@users.noreply.github.com>
2025-09-21 16:58:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -257,4 +257,5 @@ def save_settings():
         return jsonify({"success": False, "error": str(e)})
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
